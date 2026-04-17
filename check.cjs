e0399e2219a57f410a349372537b615964a0fb92
onlifitapp-lab <onlifit.app@gmail.com>
2026-04-17 15:47:38 +0530

    Convert Trainer Settings to true SPA route (/trainer/settings) with no reloads

--- check.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require("fs");
-let content = fs.readFileSync("login.html", "utf8");
-console.log(content.substring(content.indexOf("async function handleAuth"), content.indexOf("async function handleAuth") + 1500));
